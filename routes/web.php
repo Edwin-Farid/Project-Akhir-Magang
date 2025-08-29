3ffd33d8056a8269e02f2cbd9f3726d876548681
Edwin-Farid <117edwin@gmail.com>
2025-08-29 16:41:59 +0800

    fix: typo

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\MenuController;
 use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('makanan', MenuController::class);
+Route::resource('menu', MenuController::class);
 
 Route::resource('reservation', ReservationController::class);
